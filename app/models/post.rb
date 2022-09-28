b16183b83d7284da412996a149b0e014da07faca
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-09-28 19:01:04 -0400

    Create 5 methods for User, Post, Comment and Like classes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author_id, class_name: 'User'
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_save: update_post_counter
+
+  def recent_five_comments
+    Comment.last(5)
+  end
+
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
 end
