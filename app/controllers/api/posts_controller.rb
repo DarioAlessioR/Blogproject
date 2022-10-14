4f03e4797dc532946db2a966b347ba0eea316d29
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-10-14 14:19:00 -0400

    Update api posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,7 @@
 class Api::PostsController < ApplicationController
   before_action :authenticate_user!
   respond_to :json
+  
   def index
     @posts = current_user.posts
     render json: { posts: @posts }, status: :ok
